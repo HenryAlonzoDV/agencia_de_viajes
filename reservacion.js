afab3651c21d6351985614666c1b6cb7dedf9cde
Henry Alonzo <henryalonzo.dev@gmail.com>
2023-05-30 01:26:08 -0300

    capturar datos del formulario

--- reservacion.js
+++ reservacion.js
@@ -1,26 +1,40 @@
 
-
 // OBTNER VALORES
-const precioUnitario = 150
-const cantidadPasajeros = document.getElementById("cantidadPasajeros");
-const descuento = document.getElementById("descuentoPromocional");
+
+function capturarForm() {
+    const nombrePasajero = document.querySelector("#nombreForm").value;
+    const ApellidoPasajero = document.querySelector("#apellidoForm").value;
+    const emailPasajero = document.querySelector("#emailForm").value;
+    const cantidadPasajeros = parseInt (document.querySelector("#cantidadPasajeros").value);
+    const descuento = parseInt (document.querySelector("#descuentoPromocional").value);
+    const sumit = document.querySelector("#botonSumit");
+    
+    sumit.addEventListener('click', function() {
+       const datosFormulario = [nombrePasajero, ApellidoPasajero, emailPasajero, cantidadPasajeros, descuento]
+       console.log(datosFormulario)
+     });
+     
+    };
+
 
 // CALCULAR PRECIO
+
 function calcular() {
-    // PASAR LOS VALORES DE STRING A NUMBER
-    const cp = parseInt (cantidadPasajeros.value);
-    const pu = precioUnitario;
-    const des = parseInt (descuento.value);
+    const precioUnitario = 150
+    const cantidadPasajeros = parseInt (document.querySelector("#cantidadPasajeros").value);
+    const descuento = parseInt (document.querySelector("#descuentoPromocional").value);
 
     // CALCULO
-    subTotal= cp * pu;
-    promocion = subTotal * (des / 100);
+    subTotal= cantidadPasajeros * precioUnitario;
+    promocion = subTotal * (descuento / 100);
     total = subTotal - promocion;
     console.log(total);
     
     // INGRESAR RESULTADO EN EL HTML
-    const cambioPrecio = document.getElementById("precio-total");
+    const cambioPrecio = document.querySelector("#precio-total");
     cambioPrecio.innerHTML = total  
 }
+
+
 
 
