231a51968466cb3f04d67d4145d6733f29f68939
Henry Alonzo <henryalonzo.dev@gmail.com>
2023-05-31 05:24:25 -0300

    se agrego alertas al enviar o borrar el formulario

--- reservacion.js
+++ reservacion.js
@@ -1,8 +1,9 @@
 // OBTNER DATOS
 
-const submit = document.querySelector("#form");
+const formulario = document.querySelector("#form");
 const obtenerPasajeros = document.querySelector("#cantidadPasajeros");
 const obtenerDescuento = document.querySelector("#descuentoPromocional");
+const botonBorrar = document.querySelector("#botonBorrar");
 
 // CALCULO DEL PRECIO
 
@@ -17,7 +18,8 @@ function calcular() {
     subTotal= cantidadPasajeros * precioUnitario;
     promocion = subTotal * (descuento / 100);
     total = subTotal - promocion;
-    console.log(total);
+    console.log(total)
+
     
     // INGRESAR RESULTADO EN EL HTML
     const cambioPrecio = document.querySelector("#precio-total");
@@ -31,7 +33,45 @@ function datos(event) {
     const form = new FormData(this);
     const datosFormulario = [form.get("name"), form.get("lastName"), form.get("email"), form.get("pasajeros"), form.get("descuento")]
     console.log (datosFormulario);
+
+    // ALERTA
+    swal({
+        title: "Enviado con exito",
+        icon: "success"
+    });
 }
+
+// BORRAR EL FORMULARIO
+
+function borrarFormulario() {
+    const nPasajero = document.querySelector("#nombreForm").value;
+    const apPasajero = document.querySelector("#apellidoForm").value;
+    const emailPasajero = document.querySelector("#emailForm").value;
+    const pasajeros = parseInt (document.querySelector("#cantidadPasajeros").value);
+    const descuento = parseInt (document.querySelector("#descuentoPromocional").value);
+
+    // CONDICIONAL PARA LAS ALERTAS 
+    
+    if (nPasajero === "" 
+    && apPasajero === "" 
+    && emailPasajero === ""
+    && pasajeros === 0 
+    && descuento === 0)  {
+        swal({
+            title: "No hay campos para borrar",
+            icon: "error"
+        });
+    } else {
+        formulario.reset();
+        // ALERTA
+        swal({
+            title: "Formulario borrado con éxito",
+            icon: "success"
+        });
+    }
+}
+
+
 
 // EVENTOS DE ESCUCHA
 
@@ -39,4 +79,6 @@ obtenerPasajeros.addEventListener("change", calcular)
 
 obtenerDescuento.addEventListener("change", calcular)
 
-submit.addEventListener("submit", datos);
+botonBorrar.addEventListener("click", borrarFormulario)
+
+formulario.addEventListener("submit", datos);
