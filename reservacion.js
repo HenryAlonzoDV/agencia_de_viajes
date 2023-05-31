a4251478226479d921f6e824125ebb602b3c653a
Henry Alonzo <henryalonzo.dev@gmail.com>
2023-05-31 00:42:59 -0300

    mejora en el codigo js para el manejo del formulario

--- reservacion.js
+++ reservacion.js
@@ -1,25 +1,14 @@
+// OBTNER DATOS
 
-// OBTNER VALORES
+const submit = document.querySelector("#form");
+const obtenerPasajeros = document.querySelector("#cantidadPasajeros");
+const obtenerDescuento = document.querySelector("#descuentoPromocional");
 
-function capturarForm() {
-    const nombrePasajero = document.querySelector("#nombreForm").value;
-    const ApellidoPasajero = document.querySelector("#apellidoForm").value;
-    const emailPasajero = document.querySelector("#emailForm").value;
-    const cantidadPasajeros = parseInt (document.querySelector("#cantidadPasajeros").value);
-    const descuento = parseInt (document.querySelector("#descuentoPromocional").value);
-    const sumit = document.querySelector("#botonSumit");
-    
-    sumit.addEventListener('click', function() {
-       const datosFormulario = [nombrePasajero, ApellidoPasajero, emailPasajero, cantidadPasajeros, descuento]
-       console.log(datosFormulario)
-     });
-     
-    };
-
-
-// CALCULAR PRECIO
+// CALCULO DEL PRECIO
 
 function calcular() {
+
+    // PASAR A NUMEROS
     const precioUnitario = 150
     const cantidadPasajeros = parseInt (document.querySelector("#cantidadPasajeros").value);
     const descuento = parseInt (document.querySelector("#descuentoPromocional").value);
@@ -32,9 +21,22 @@ function calcular() {
     
     // INGRESAR RESULTADO EN EL HTML
     const cambioPrecio = document.querySelector("#precio-total");
-    cambioPrecio.innerHTML = total  
+    cambioPrecio.innerText = total  
 }
 
+// ENVIAR FORMULARIO
 
+function datos(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+    const datosFormulario = [form.get("name"), form.get("lastName"), form.get("email"), form.get("pasajeros"), form.get("descuento")]
+    console.log (datosFormulario);
+}
 
+// EVENTOS DE ESCUCHA
 
+obtenerPasajeros.addEventListener("change", calcular)
+
+obtenerDescuento.addEventListener("change", calcular)
+
+submit.addEventListener("submit", datos);
